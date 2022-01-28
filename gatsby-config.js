bb8cd9348df107ee81b62dadd07cbceae80e19fa
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-01-28 18:29:39 +0530

    null added

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
             resolve: 'gatsby-remark-external-links',
             options: {
               target: '_blank',
-              rel: 'follow noopener norefferer',
+              rel: null,
             },
           },
           {
