8d7b09a06261465b583d65c121790957eba9adba
anishpras <anishprashun118@gmail.com>
2025-08-04 17:06:17 +0530

    404

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -13,16 +13,65 @@ export function NotFound({ className }: { className?: string }) {
       )}
     >
       <svg
-        className="h-28 w-full"
+        width="512"
+        height="256"
+        viewBox="0 0 512 256"
+        fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 514 258"
-        fill="none"
       >
-        <path
-          d="M65 193v64h128v-64H65Zm0 0H1V65h64m0 128V65m384 0H321v128h128m0-128V1H257v256h192v-64m0-128v128m0-128h64v128h-64M65 65h128V1H65v64Z"
-          stroke="currentColor"
-          strokeWidth="1"
-          vectorEffect="non-scaling-stroke"
+        <rect
+          x="174.15"
+          y="256"
+          width="209.997"
+          height="55.7279"
+          transform="rotate(-90 174.15 256)"
+          fill="currentColor"
+        />
+        <rect
+          y="255.617"
+          width="209.273"
+          height="55.7279"
+          transform="rotate(-90 0 255.617)"
+          fill="currentColor"
+        />
+        <rect
+          x="55.7279"
+          width="118.422"
+          height="46.3441"
+          fill="currentColor"
+        />
+        <rect
+          x="55.7279"
+          y="117.309"
+          width="118.422"
+          height="46.3441"
+          fill="currentColor"
+        />
+        <rect
+          x="286.648"
+          y="0.743164"
+          width="168.071"
+          height="47.5439"
+          fill="currentColor"
+        />
+        <rect
+          width="255.38"
+          height="56.9114"
+          transform="matrix(-0.0164782 0.999864 -0.999934 -0.0115002 343.238 0.654785)"
+          fill="currentColor"
+        />
+        <rect
+          x="283.98"
+          y="128.518"
+          width="170.739"
+          height="47.5439"
+          fill="currentColor"
+        />
+        <rect
+          width="80.0702"
+          height="56.9129"
+          transform="matrix(-0.00424694 -0.999991 0.999996 -0.00296377 455.087 128.416)"
+          fill="currentColor"
         />
       </svg>
 
