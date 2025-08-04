aedba305dab82b676deea85cf08ddf4cb065e5a9
anishpras <anishprashun118@gmail.com>
2025-08-04 17:17:01 +0530

    updated

--- src/app/(llms)/llms.txt/route.ts
+++ src/app/(llms)/llms.txt/route.ts
@@ -5,7 +5,7 @@ const allPosts = getAllPosts();
 
 const content = `# anishprashun.me
 
-> A minimal portfolio, component registry, and blog to showcase my work as a Design Engineer.
+> A minimal portfolio, and blog to showcase my work as a Software Engineer.
 
 - [About](${SITE_INFO.url}/about.md): A quick intro to me, my tech stack, and how to connect.
 - [Experience](${SITE_INFO.url}/experience.md): Highlights from my career and key roles I've taken on.
